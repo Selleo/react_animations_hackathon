b3fb159b90317c94975fb969526c50256932d69e
Marzena Orawczak <marzena.orawczak@gmail.com>
2022-09-16 12:24:55 +0200

    feat: handled deleting items

--- src/pages/react-motion/List/index.js
+++ src/pages/react-motion/List/index.js
@@ -1,23 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { presets, spring } from 'react-motion'
-import { StaggeredMotion } from 'react-motion/lib/react-motion'
+import { Motion, StaggeredMotion } from 'react-motion/lib/react-motion'
+import { useLocalStorage } from 'react-use'
 
 const listItems = [
-  { id: 1, title: 'Item 1' },
-  { id: 2, title: 'Item 2' },
-  { id: 3, title: 'Item 3' },
-  { id: 4, title: 'Item 4' },
-  { id: 5, title: 'Item 5' },
-  { id: 6, title: 'Item 6' },
-  { id: 7, title: 'Item 7' },
-  { id: 8, title: 'Item 8' },
-  { id: 9, title: 'Item 9' },
-  { id: 10, title: 'Item 10' },
-  { id: 11, title: 'Item 11' },
+  { id: 1, title: 'Item 1', description: 'Description' },
+  { id: 2, title: 'Item 2', description: 'Description' },
+  { id: 3, title: 'Item 3', description: 'Description' },
+  { id: 4, title: 'Item 4', description: 'Description' },
+  { id: 5, title: 'Item 5', description: 'Description' },
+  { id: 6, title: 'Item 6', description: 'Description' },
+  { id: 7, title: 'Item 7', description: 'Description' },
+  { id: 8, title: 'Item 8', description: 'Description' },
+  { id: 9, title: 'Item 9', description: 'Description' },
+  { id: 10, title: 'Item 10', description: 'Description' },
+  { id: 11, title: 'Item 11', description: 'Description' },
 ]
 
 const List = () => {
-  const [state, setState] = useState({ x: 400, y: 300 })
+  const [state] = useState({ x: 400, y: 50 })
+  const [globalState, setGlobalState] = useState()
+  const [localStorageItems, setValue] = useLocalStorage('listItems', listItems)
+
   const getStyles = (prevStyles) => {
     // `prevStyles` is the interpolated value of the last tick
     const endValue = prevStyles.map((item, i) => {
@@ -30,38 +34,96 @@ const List = () => {
     })
     return endValue
   }
+
+  const handleDeleteItem = (itemId) => {
+    //doesn't work due to <StaggeredMotion> is only for fixed length lists
+    const newItems = localStorageItems.filter((item) => item.id !== itemId)
+    setValue(newItems)
+  }
+
+  const handleAnimatadRemove = (itemIndex, items) => {
+    const copyState = [...items]
+    const oldState = copyState[itemIndex]
+    const newState = {
+      ...oldState,
+      x: spring(-800, { stiffness: 160, damping: 25 }),
+      clicked: true,
+    }
+    copyState[itemIndex] = newState
+    setGlobalState(copyState)
+  }
   return (
     <>
       <StaggeredMotion
-        defaultStyles={listItems.map((item) => ({ x: 0, y: 0 }))}
+        defaultStyles={localStorageItems.map((item) => ({
+          x: 0,
+          y: 0,
+        }))}
         styles={getStyles}
       >
-        {(items) => (
-          <div className="demo1">
-            {items.map(({ x, y }, i) => (
-              <div
-                key={i}
-                className={`demo1-ball ball-${i}`}
-                style={{
-                  WebkitTransform: `translate3d(${x - 25}px, ${y - 25}px, 0)`,
-                  transform: `translate3d(${x - 25}px, ${y - 25}px, 0)`,
-                  zIndex: items.length - i,
-                  height: '50px',
-                  width: '100px',
-                  background: i % 2 === 0 ? 'hotpink' : 'greenyellow',
-                }}
-              >
-                {listItems[i].title}
-              </div>
-            ))}
-          </div>
-        )}
+        {(items) => {
+          return (
+            <div className="demo1">
+              {items.map(({ x, y }, i) => {
+                return (
+                  <Motion
+                    key={i}
+                    defaultStyle={{ x: x, y: y }}
+                    style={
+                      globalState
+                        ? { x: globalState[i].x, y: globalState[i].y }
+                        : { x: x, y: y }
+                    }
+                    onRest={() => {
+                      if (globalState && globalState[i]?.clicked)
+                        handleDeleteItem(localStorageItems[i].id)
+                    }}
+                  >
+                    {(interpolatingStyle) => (
+                      <div
+                        className={`demo1-ball ball-${i}`}
+                        style={{
+                          WebkitTransform: `translate3d(${
+                            interpolatingStyle.x - 25
+                          }px, ${interpolatingStyle.y - 25}px, 0)`,
+                          transform: `translate3d(${
+                            interpolatingStyle.x - 25
+                          }px, ${interpolatingStyle.y - 25}px, 0)`,
+                          zIndex: items.length - i,
+                          height: '50px',
+                          display: 'flex',
+                          width: '300px',
+                          justifyContent: 'space-between',
+                        }}
+                      >
+                        <div
+                          style={{
+                            background: i % 2 === 0 ? 'hotpink' : 'greenyellow',
+                            flexGrow: 1,
+                          }}
+                        >
+                          {localStorageItems && localStorageItems[i]?.title}
+                        </div>
+                        <button
+                          type="button"
+                          style={{
+                            backgroundColor: 'red',
+                            marginLeft: '12px',
+                            padding: '5px',
+                          }}
+                          onClick={() => handleAnimatadRemove(i, items)}
+                        >
+                          Remove
+                        </button>
+                      </div>
+                    )}
+                  </Motion>
+                )
+              })}
+            </div>
+          )
+        }}
       </StaggeredMotion>
-      {/*<button*/}
-      {/*  onClick={() => setState(({ x, y }) => ({ x: x + 50, y: y - 10 }))}*/}
-      {/*>*/}
-      {/*  xd*/}
-      {/*</button>*/}
     </>
   )
 }
