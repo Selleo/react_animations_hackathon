f104abea6fd50eb4931ad01208072c0d65ccf711
Arkadiusz Machalica <a.machalica@selleo.com>
2022-09-16 15:40:14 +0200

    Add skew

--- src/pages/react-spring/Main.js
+++ src/pages/react-spring/Main.js
@@ -1,9 +1,11 @@
 import { List } from './List'
+import Loader from './Loader'
 import { TextField } from './TextField'
 
 export const Main = () => {
   return (
     <div>
+      <Loader />
       React spring
       <TextField />
       <br />
