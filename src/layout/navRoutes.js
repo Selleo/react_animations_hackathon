c12af60cb2d24221271830abf05b5d8777eb6389
Michał Klejdysz <80550573+michalklejdysz@users.noreply.github.com>
2022-09-16 10:17:50 +0200

    [React Motion] Add input placeholder animation

--- src/layout/navRoutes.js
+++ src/layout/navRoutes.js
@@ -1,6 +1,7 @@
 import {
   FramerMotionPage,
   GSAPPage,
+  ReactMotionInput,
   ReactMotionPage,
   ReactSpringPage,
 } from 'pages'
@@ -8,6 +9,11 @@ import {
 export const navRoutes = [
   { name: 'framer-motion', to: '/framer-motion', Component: FramerMotionPage },
   { name: 'react-motion', to: '/react-motion', Component: ReactMotionPage },
+  {
+    name: 'react-motion-inputs',
+    to: '/react-motion-input',
+    Component: ReactMotionInput,
+  },
   { name: 'react-spring', to: '/react-spring', Component: ReactSpringPage },
   { name: 'gsap', to: '/gsap', Component: GSAPPage },
 ]
